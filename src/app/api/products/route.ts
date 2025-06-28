0a9e620c76b64edf2c1080c600e1518209cf66ca
Stiff <stiveson02@gmail.com>
2025-06-28 18:28:18 -0500

    route productos get

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from '@prisma/client';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
 const prisma = new PrismaClient();
 
@@ -45,3 +45,25 @@ export async function POST(req: Request) {
 
   return NextResponse.json(finalProducts);
 }
+
+export async function GET(req: NextRequest) {
+  const { searchParams } = new URL(req.url);
+  const idParam = searchParams.get('id');
+
+  const id = idParam ? parseInt(idParam, 10) : NaN;
+
+  if (isNaN(id)) {
+    return NextResponse.json({ error: 'ID inválido' }, { status: 400 });
+  }
+
+  const product = await prisma.product.findUnique({
+    where: { id },
+  });
+
+  if (!product) {
+    return NextResponse.json({ error: 'Producto no encontrado' }, { status: 404 });
+  }
+
+  return NextResponse.json(product);
+}
+
